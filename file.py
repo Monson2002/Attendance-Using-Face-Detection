ccafda3fbaa9b3b97297555435c727b685e3a5a7
monson2002 <monsonreji2002@gmail.com>
2023-08-23 19:42:28 +0530

    7:41pm 23-8-23 Partitioning to training/testing

--- file.py
+++ file.py
@@ -33,32 +33,70 @@ ANC_PATH = os.path.join('data', 'anchor')
 #         os.replace(OLD_PATH, NEW_PATH)
 # print('Done')
 
-# Capturing images from webcam using Open CV
-cap = VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while cap.isOpened():
-    ret, frame = cap.read()
+'''To be used for capturing images from webcam'''
+# # Capturing images from webcam using Open CV
+# cap = VideoCapture(0)
+# if not cap.isOpened():
+#     print("Cannot open camera")
+#     exit()
+# while cap.isOpened():
+#     ret, frame = cap.read()
     
-    # Setting frame size
-    frame = frame[120:120+250, 200:200+250, :]
+#     # Setting frame size
+#     frame = frame[120:120+250, 200:200+250, :]
     
-    # Filling positive images
-    if cv2.waitKey(1) == ord('p'):
-        image = os.path.join(POS_PATH, str(uuid.uuid1()) + '.jpg')
-        cv2.imwrite(image, frame)
+#     # Filling positive images
+#     if cv2.waitKey(1) == ord('p'):
+#         image = os.path.join(POS_PATH, str(uuid.uuid1()) + '.jpg')
+#         cv2.imwrite(image, frame)
         
-    # Filling anchor images
-    if cv2.waitKey(1) == ord('a'):
-        image = os.path.join(ANC_PATH, str(uuid.uuid1()) + '.jpg')
-        cv2.imwrite(image, frame)
+#     # Filling anchor images
+#     if cv2.waitKey(1) == ord('a'):
+#         image = os.path.join(ANC_PATH, str(uuid.uuid1()) + '.jpg')
+#         cv2.imwrite(image, frame)
         
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# Release the camera
-cap.release()
-cv2.destroyAllWindows()
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+# # Release the camera
+# cap.release()
+# cv2.destroyAllWindows()
 
-print(plt.imshow(frame))
+anchor = tf.data.Dataset.list_files(os.path.join(ANC_PATH, '*.jpg')).take(300)
+positive = tf.data.Dataset.list_files(os.path.join(POS_PATH, '*.jpg')).take(300)
+negative = tf.data.Dataset.list_files(os.path.join(NEG_PATH, '*.jpg')).take(300)
+
+def preprocess(file_path):
+    # Storing the image to a variable
+    image = tf.io.read_file(file_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    
+    # Resizingthe image to 105x105
+    image = tf.image.resize(image, [105, 105])
+    
+    # Converting pixel values to [0-1]
+    image = image / 255.0
+    return image
+    
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives)
+
+def preprocess_two_imgs(input_img, validation_img, label):
+    return (preprocess(input_img), preprocess(validation_img), label)
+
+# Buiding a data loader pipeline
+data = data.map(preprocess_two_imgs)
+data = data.cache()
+data = data.shuffle(1024)
+
+# Training partition
+train_data = data.take(round(len(data) * .7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+# Testing partition
+test_data = data.skip(round(len(data) * .7))
+test_data = test_data.take(round(len(data) * .3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
